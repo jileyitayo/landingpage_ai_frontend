0d1d0285aa32e28a62377fa667d6aca2351e55b2
Jileyitayo <jileyitayo@gmail.com>
2025-05-04 22:42:49 +0100

    Revert "Moved from app to src/app"
    This reverts commit c08dadce29a5f80223fd477d4d84415abf52c59b.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Chat from '@/components/ui/chat';
 import { useEffect, useState } from 'react';
 
 export default function Home() {
@@ -18,8 +17,6 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-24">
             <h1 className="text-4xl font-bold">Message from Backend: {message}</h1>
-            <p className="text-lg mt-4">This is a simple chat application.</p>
-            <Chat/>
         </main>
     );
 }
